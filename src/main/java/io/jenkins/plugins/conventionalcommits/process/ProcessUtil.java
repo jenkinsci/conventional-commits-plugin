bcc51a13d6d15962f37536b13053bbac333deba2
philippart-s <stephane.philippart@harmonie-mutuelle.fr>
2021-10-13 09:12:53 +0200

    test: add some tests for ProcessUtil

--- src/main/java/io/jenkins/plugins/conventionalcommits/process/ProcessUtil.java
+++ src/main/java/io/jenkins/plugins/conventionalcommits/process/ProcessUtil.java
@@ -1,9 +1,11 @@
 package io.jenkins.plugins.conventionalcommits.process;
 
-import static io.jenkins.plugins.conventionalcommits.NextVersionStep.stdout;
-
+import com.google.common.io.LineReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Class to execute some CLI commands.
@@ -11,14 +13,33 @@ import java.io.IOException;
 public class ProcessUtil {
 
   /**
+   * Reads data from stdout.
+   *
+   * @param in InputStream object.
+   * @return read data.
+   * @throws IOException If an error occur reading files.
+   */
+  private static String stdout(InputStream in) throws IOException {
+    StringBuilder builder = new StringBuilder();
+    LineReader reader = new LineReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+    while (true) {
+      String line = reader.readLine();
+      if (line == null) {
+        break;
+      }
+      builder.append(line);
+      builder.append(System.getProperty("line.separator"));
+    }
+    return builder.toString();
+  }
+
+  /**
    * Execute a CLI command using ProcessBuilder.
    *
-   * @param dir Directory where execute the command.
+   * @param dir            Directory where execute the command.
    * @param commandAndArgs Command and parameters of the command.
-   *
    * @return THe output of the command.
-   *
-   * @throws IOException If an error occur accessing files.
+   * @throws IOException          If an error occur accessing files.
    * @throws InterruptedException If the command is interrupted.
    */
   public static String execute(File dir, String... commandAndArgs)
