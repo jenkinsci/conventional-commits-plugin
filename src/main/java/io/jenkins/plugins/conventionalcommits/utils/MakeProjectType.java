d8a9f328b48bdd6295f51cb6ba0568266392ae35
EtienneG <guenyot.etienne@gmail.com>
2021-10-26 23:55:06 +0200

    mutualised createNewUpdateFile in the abstract class

--- src/main/java/io/jenkins/plugins/conventionalcommits/utils/MakeProjectType.java
+++ src/main/java/io/jenkins/plugins/conventionalcommits/utils/MakeProjectType.java
@@ -2,14 +2,9 @@ package io.jenkins.plugins.conventionalcommits.utils;
 
 import com.github.zafarkhaja.semver.Version;
 import io.jenkins.plugins.conventionalcommits.process.ProcessHelper;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -52,44 +47,21 @@ public class MakeProjectType extends ProjectType {
       throws IOException, InterruptedException {
     Objects.requireNonNull(directory);
     Objects.requireNonNull(nextVersion);
-    // Line to read
-    String line;
-    // Flag to know if a version tag is in the Makefile
-    boolean isVersionTag = false;
+
     // Absolute path to the Makefile
     String buildTempPath = String.format("%s%sMakefile.temp", directory.getAbsolutePath(),
         File.separator);
     // Absolute path to the Makefile
     String buildPath = String.format("%s%sMakefile", directory.getAbsolutePath(), File.separator);
 
-    String currentVersion = "";
+    //Version type to match
+    String[] versionToMatch = new String[] {"version", "version:", "version :"};
 
-    try (BufferedReader reader = Files.newBufferedReader(Paths.get(buildPath))) {
-      try (BufferedWriter fw = Files.newBufferedWriter(Paths.get(buildTempPath), 
-          StandardCharsets.UTF_8)) {
+    // Flag to know if a version tag is in the Makefile
+    boolean isVersionFound =
+            createNewUpdateFile(buildPath, buildTempPath, nextVersion, false, versionToMatch);
 
-        while ((line = reader.readLine()) != null) {
-          if (!isVersionTag & (line.toLowerCase().startsWith("version ") 
-              || line.toLowerCase().startsWith("version")
-              || line.toLowerCase().startsWith("version:") 
-              || line.toLowerCase().startsWith("version :"))) {
-            String[] words = line.split("=");
-            currentVersion = words[1].trim();
-            fw.write(String.format("%s%n", line.replace(currentVersion, nextVersion.toString())));
-            isVersionTag = true;
-          } else {
-            fw.write(String.format("%s%n", line));
-          }
-        }
-      }
-    }
-
-    if (isVersionTag) {
-      // Replace Makefile with updated version
-      Files.move(Paths.get(buildTempPath), Paths.get(buildPath), 
-          StandardCopyOption.REPLACE_EXISTING);
-    } else {
-      Files.deleteIfExists(Paths.get(buildPath));
+    if (!isVersionFound) {
       throw new IOException("Unable to get version in Makefile");
     }
   }
--- src/main/java/io/jenkins/plugins/conventionalcommits/utils/ProjectType.java
+++ src/main/java/io/jenkins/plugins/conventionalcommits/utils/ProjectType.java
@@ -2,8 +2,16 @@ package io.jenkins.plugins.conventionalcommits.utils;
 
 import com.github.zafarkhaja.semver.Version;
 import io.jenkins.plugins.conventionalcommits.process.ProcessHelper;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 abstract class ProjectType {
 
@@ -15,4 +23,58 @@ abstract class ProjectType {
   public abstract void writeVersion(
       File directory, Version nextVersion, ProcessHelper processHelper)
       throws IOException, InterruptedException;
+
+  /**
+   * Write an updated temporary file with next version
+   * then replace the project config file with it.
+   * Return if a version tag was found.
+   *
+   * @param buildPath Path of the file updated
+   * @param buildTempPath Path of a temporary file to replace buildPath
+   * @param nextVersion Version to update config file
+   * @throws IOException If errors occurs when write the file
+   */
+  public boolean createNewUpdateFile(String buildPath, String buildTempPath, Version nextVersion,
+                                  boolean isIdented, String[] matchingWords)
+          throws IOException {
+    // Line to read
+    String line;
+    // Flag to know if a version tag is in the config file
+    boolean isVersionTag = false;
+
+    String currentVersion;
+
+    try (BufferedReader reader = Files.newBufferedReader(Paths.get(buildPath))) {
+      try (BufferedWriter fw = Files.newBufferedWriter(Paths.get(buildTempPath),
+              StandardCharsets.UTF_8)) {
+        Stream<String> streamOfWord = Arrays.stream(matchingWords);
+        while ((line = reader.readLine()) != null) {
+          if (!isVersionTag & streamOfWord
+                  .anyMatch(isIdented ? line.toLowerCase()::contains :
+                          line.toLowerCase()::startsWith)) {
+            String[] words = line.split("=");
+            currentVersion = words[1].trim();
+            if (currentVersion.contains("\"")) {
+              fw.write(String.format("%s%n", line.replace(currentVersion, "\""
+                      + nextVersion.toString() + "\"")));
+            } else {
+              fw.write(String.format("%s%n", line.replace(currentVersion, nextVersion.toString())));
+            }
+            isVersionTag = true;
+          } else {
+            fw.write(String.format("%s%n", line));
+          }
+        }
+      }
+
+      if (isVersionTag) {
+        // Replace config with updated version
+        Files.move(Paths.get(buildTempPath), Paths.get(buildPath),
+                StandardCopyOption.REPLACE_EXISTING);
+      } else {
+        Files.deleteIfExists(Paths.get(buildPath));
+      }
+    }
+    return isVersionTag;
+  }
 }
