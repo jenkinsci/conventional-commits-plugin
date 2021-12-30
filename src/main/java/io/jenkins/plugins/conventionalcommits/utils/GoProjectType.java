3788f12f9dbe5e913928ac45b0890a095c79b205
Kris Stern <kasternhk@gmail.com>
2021-12-31 00:15:57 +0800

    feat: Add support to Go projects for reading the current version (#156)
    * Enable Go support for reading the current module version
    
    * Change to use LogUtils for logging and debug

---
+++ src/main/java/io/jenkins/plugins/conventionalcommits/utils/GoProjectType.java
@@ -0,0 +1,93 @@
+package io.jenkins.plugins.conventionalcommits.utils;
+
+import com.github.zafarkhaja.semver.Version;
+import io.jenkins.plugins.conventionalcommits.process.ProcessHelper;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * Represents a Go project type.
+ * Projects with the go.mod file are supported.
+ */
+public class GoProjectType extends ProjectType {
+
+  private boolean checkGoMod(File directory) {
+    return new File(directory, "go.mod").exists();
+  }
+
+  /**
+   * Checks if the go.mod config file exists at the root directory.
+   *
+   * @param directory The directory in which the go.mod file is searched for.
+   * @return A boolean true / false is returned.
+   */
+  @Override
+  public boolean check(File directory) {
+    return checkGoMod(directory);
+  }
+
+  /**
+   * Gets the current version of the Go module as indicated by release tag on GitHub.
+   *
+   * @param directory The directory in which the go.mod file is ideally found.
+   * @param processHelper The helper to run the "go list" command.
+   * @return The current version of the Go module concerned.
+   * @throws IOException If an error is thrown while file is read.
+   * @throws InterruptedException If an error is thrown due to interruption.
+   */
+  @Override
+  public Version getCurrentVersion(File directory, ProcessHelper processHelper)
+      throws IOException, InterruptedException {
+
+    String commandName = "go";
+
+    String result = "";
+
+    // Compiles the regex to create desired version pattern for finding the current version
+    String versionRegex = "v[0-9]+.[0-9]+.[0-9]+(-((\\balpha\\b)|(\\bbeta\\b)).[0-9])?";
+    Pattern pattern = Pattern.compile(versionRegex);
+
+    if (checkGoMod(directory)) {
+      List<String> command = Arrays.asList(commandName, "list", "-m", "-versions");
+      String longResult = processHelper.runProcessBuilder(directory, command);
+      Matcher match = pattern.matcher(longResult);
+      // The last version should be the most current version
+      // The result variable is overwritten by the last version in the while loop
+      while (match.find()) {
+        result = match.group();
+      }
+    }
+    result = result.substring(1);
+    return Version.valueOf(result.trim());
+  }
+
+  /**
+   * Check if there is a Go config file (go.mod) file in the directory.
+   * If that's the case change the file will remain the same.
+   * This is because for Go modules the release version is from GitHub repository tags.
+   *
+   * @param directory The directory to which the file is written.
+   * @param nextVersion The next version to use.
+   * @param processHelper The helper to run the command (not used here).
+   * @throws NotImplementedException If not implemented means the Go project is not supported.
+   * @throws IOException If an error is thrown while file is read.
+   * @throws InterruptedException If an error is thrown due to interruption.
+   */
+  @Override
+  public void writeVersion(File directory, Version nextVersion, ProcessHelper processHelper)
+      throws IOException, InterruptedException, NotImplementedException {
+    if (checkGoMod(directory)) {
+      String message = "The go.mod file already exists";
+      LogUtils logger = new LogUtils();
+      logger.log(Level.INFO, Level.INFO, Level.FINE, Level.FINE, true, message);
+    } else {
+      throw new NotImplementedException("Project not supported");
+    }
+  }
+}
---
+++ src/test/java/io/jenkins/plugins/conventionalcommits/utils/GoProjectTypeTest.java
@@ -0,0 +1,66 @@
+package io.jenkins.plugins.conventionalcommits.utils;
+
+import com.github.zafarkhaja.semver.Version;
+import io.jenkins.plugins.conventionalcommits.process.ProcessHelper;
+import org.hamcrest.core.IsEqual;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GoProjectTypeTest {
+  @Rule
+  public TemporaryFolder rootFolder = new TemporaryFolder();
+
+  @Mock
+  private ProcessHelper mockProcessHelper;
+
+  private void createGoMod(File goDir) throws Exception {
+    Files.deleteIfExists(Paths.get(goDir.getPath() + File.separator + "go.mod"));
+    File goMod = rootFolder.newFile(goDir.getName() + File.separator + "go.mod");
+    String configContent =
+      "module github.com/slim-patchy/hey\n" +
+        "\n" +
+        "require (\n" +
+        "\tgolang.org/x/net v0.0.0-20191009170851-d66e71096ffb\n" +
+        "\tgolang.org/x/text v0.3.2 // indirect\n" +
+        ")\n" +
+        "\n" +
+        "go 1.13\n";
+    FileWriter goWriter = new FileWriter(goMod);
+    goWriter.write(configContent);
+    goWriter.close();
+  }
+
+  @Test
+  public void shouldGetCurrentVersionForAGoMod() throws Exception {
+    // Given a Go project with a go.mod
+    File goDir = rootFolder.newFolder("SampleGoProject");
+    createGoMod(goDir);
+    when(mockProcessHelper.runProcessBuilder(any(), any())).thenReturn("go list -m: " +
+        "loading module retractions for github.com/krisstern/hey@v0.1.0: " +
+        "verifying go.mod: github.com/krisstern/hey@v0.1.4/go.mod: " +
+        "initializing sumdb.Client: " +
+        "open /opt/homebrew/bin/go/pkg/sumdb/sum.golang.org/latest: " +
+        "not a directory");
+
+    // Asking to have the current version of the project
+    GoProjectType goProjectType = new GoProjectType();
+    Version readVersion = goProjectType.getCurrentVersion(goDir, mockProcessHelper);
+
+    // The current version is returned
+    assertThat(readVersion, IsEqual.equalTo(Version.valueOf("0.1.4")));
+  }
+}
